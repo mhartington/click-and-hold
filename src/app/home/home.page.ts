42d66766a7ad26e5e8fc11a4f8eca10f5c9fca45
Mike Hartington <mikehartington@gmail.com>
2020-12-18 17:47:42 -0500

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  test() {
+    console.log('action fired!');
+  }
 }
